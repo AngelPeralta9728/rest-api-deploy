dcd6f5aab80ccd34c4200f831aa5d07262dfd24c
GastonPeraltaASAP <gperalta@asap-consulting.net>
2025-08-01 23:23:59 -0300

    commit 1/8

--- app.js
+++ app.js
@@ -1,108 +1,25 @@
-const express = require('express') //require ---> commonJS
-const crypto = require('node:crypto')
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./Schemas/movies')
+import express, { json } from 'express' //require ---> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+//como leer un json en ESModules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
+//en el futuro: el import del json sera asi:
+//import movies from './movies.json' with { type: 'json' }
+
+//como leer un json en ESModules recomendado por ahora
+
+
+
 
 const app = express()
-app.use(express.json())
+app.use(json())
+app.use(corsMiddleware())
 app.disable('x-powered-by') // dehabilitar el header x-powered-by: express
-
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234',
-    'https://movies.com',
-]
 //todos los recursos que sean MOVIES se identifican con /movies
-app.get('/movies', (req, res) => {
-    const origin = req.header('origin')
-    if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-    }
+app.use('/movies', moviesRouter)
 
-    const { genre } = req.query
-    if(genre) {
-        const filteredMovies = movies.filter(
-            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-        )
-        return res.json(filteredMovies)
-        }
-    res.json(movies)
-})
-
-app.get('/movies/:id', (req,res) => { //path-to-regexp
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-        res.status(404).json({ message: 'Movie not found' })
-})
-
-app.post('/movies', (req, res) => {
- const result = validateMovie(req.body)
-
- if (result.error) {
-    return res.status(400).json({error: JSON.parse(result.error.message) })
- }
-
-    const newMovie = {
-        id: crypto.randomUUID(),
-        ...result.data
-    }
-
-    movies.push(newMovie)
-    res.status(201).json(newMovie)
-})
-
-app.delete('/movies/:id', (req, res) => {
-        const origin = req.header('origin')
-        if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-        }
-    
-
-
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: 'Movie not found'})
-    }
-
-    movies.splice(movieIndex, 1)
-
-    return res.json({ message: 'Movie deleted '})
-})
-
-app.patch('/movies/:id', (req, res) => {
-    const result = validatePartialMovie(req.body)
-
-    if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: 'Movie not found' })
-    }
-const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-}
-
- movies[movieIndex] = updateMovie
-
- return res.json(updateMovie)
-})
-
-app.options('/movies/:id', (req, res) => {
-        const origin = req.header('origin')
-        if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-        }
-        res.send(200)
-})
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, '0.0.0.0', () =>{
